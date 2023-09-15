4704844b4cff64a9977d26cf5758e158b2991f7f
Delta141a <139953536+Delta141a@users.noreply.github.com>
2023-09-15 11:14:43 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1694751160,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/grond-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/gold-sheet0.png",
+		"images/gold-sheet1.png",
+		"images/gold-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
